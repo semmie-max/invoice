4c4c8fc7bb1e51751f7a7f05f5692087d043ae24
semmie-max <semmie162l@gmail.com>
2026-08-16 21:14:18 +0100

    fix custom domain routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>
+    </BrowserRouter>
   </StrictMode>,
 )
